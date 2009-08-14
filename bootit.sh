eb8b02a6dd4e0f4c6107e62661701ef8e8cda089
pbutler <pbutler@ea86384c-4743-4ab3-b769-c76ea7e5d1af>
2009-08-14 21:07:04 +0000

    added some gitconfig voodoo and a screenrc
    git-svn-id: file:///home/pbutler/svn/bootstrap@23 ea86384c-4743-4ab3-b769-c76ea7e5d1af

--- bootit.sh
+++ bootit.sh
@@ -4,6 +4,7 @@ svn update
 cp -f bashrc ~/.bashrc
 cp -f ssh_config ~/.ssh/config
 cp -f vimrc ~/.vimrc
+cp -f screenrc ~/.screenrc
 cp -f gitconfig ~/.gitconfig
 cp -f vimperatorrc ~/.vimperatorrc
 if [ ! -d ~/bin ]; then
