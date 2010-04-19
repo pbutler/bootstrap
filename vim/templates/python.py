591ce20a3220779fb31e21983535ed10fac03dc2
pbutler <pbutler@ea86384c-4743-4ab3-b769-c76ea7e5d1af>
2010-04-19 19:04:01 +0000

    updated python template to have simple optparse example in it
    git-svn-id: file:///home/pbutler/svn/bootstrap@48 ea86384c-4743-4ab3-b769-c76ea7e5d1af

--- vim/templates/python.py
+++ vim/templates/python.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # vim: ts=4 sts=4 sw=4 tw=79 sta et
-"""
+"""%prog [options]
 Python source code - replace this with a description of the code and write the code below this text.
 """
 
@@ -9,8 +9,20 @@ __author__ = 'Patrick Butler'
 __email__  = 'pbutler@killertux.org'
 
 def main(args):
+    import  optparse
+    parser = optparse.OptionParser()
+    parser.usage = __doc__
+    parser.add_option("-q", "--quiet",
+                      action="store_false", dest="verbose", default=True,
+                      help="don't print status messages to stdout")
+    (options, args) = parser.parse_args()
+    if len(args) < 0:
+        parser.error("Not enough arguments given")
     return 0
+
+
 
 if __name__ == "__main__":
     import sys
     sys.exit( main( sys.argv ) )
+
