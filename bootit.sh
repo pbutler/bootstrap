7af61b03d346b1313a5d0fd2be54d7e70345037a
pbutler <pbutler@ea86384c-4743-4ab3-b769-c76ea7e5d1af>
2011-01-08 19:32:47 +0000

    changed bootit.sh to save file attributes on copies
    git-svn-id: file:///home/pbutler/svn/bootstrap@60 ea86384c-4743-4ab3-b769-c76ea7e5d1af

--- bootit.sh
+++ bootit.sh
@@ -11,13 +11,13 @@ if [ ! -d ~/.ssh ]; then
 	mkdir -p -m 0700 ~/.ssh
 fi
 
-cp -f bashrc ~/.bashrc
-cp -f ssh_config ~/.ssh/config
-cp -f vimrc ~/.vimrc
-cp -f screenrc ~/.screenrc
-cp -f gitconfig ~/.gitconfig
-cp -f vimperatorrc ~/.vimperatorrc
-cp -f Xdefaults ~/.Xdefaults
+cp -f -p bashrc ~/.bashrc
+cp -f -p ssh_config ~/.ssh/config
+cp -f -p vimrc ~/.vimrc
+cp -f -p screenrc ~/.screenrc
+cp -f -p gitconfig ~/.gitconfig
+cp -f -p vimperatorrc ~/.vimperatorrc
+cp -f -p Xdefaults ~/.Xdefaults
 
 chmod 0600 ~/.ssh/config
 
@@ -29,20 +29,22 @@ cp -f xterm-256color ~/.terminfo/78/xterm-256color
 if [ ! -d ~/bin ]; then
 	mkdir ~/bin
 fi
-cp -r bin/* ~/bin
+
+cp -r -p bin/* ~/bin
+
 if [ ! -d ~/.vim ]; then
 	mkdir ~/.vim
 fi
 
 if [ ! -d ~/.vim/tmp ]; then
-	mkdir ~/.vim/tmp
+	mkdir -p ~/.vim/tmp
 	chmod 700 ~/.vim/tmp
 fi
 
 if [ -x `which svn` ]; then
 	svn --force export vim/ ~/.vim
 else
-	cp -r vim/* ~/.vim
+	cp -p -r vim/* ~/.vim
 	find ~/.vim -type d -name .svn -exec rm -rf "{}" \;
 fi
 
@@ -50,9 +52,9 @@ if [ `uname -s` = "Darwin" ]; then
 	echo "OS X detected installing applicable files..."
 	if [ -x ~/.cabal/bin/xmonad ]; then
 		echo "Installing Xmonad config..."
-		cp -f osx/Xmodmap ~/.Xmodmap
-		cp -f osx/xinitrc ~/.xinitrc
+		cp -f -p osx/Xmodmap ~/.Xmodmap
+		cp -f -p osx/xinitrc ~/.xinitrc
 		mkdir -p ~/.xmonad
-		cp osx/xmonad.hs ~/.xmonad/xmonad.hs
+		cp -p osx/xmonad.hs ~/.xmonad/xmonad.hs
 	fi
 fi
