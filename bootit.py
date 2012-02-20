2af9be11e1017ccd8f3793902aed7a2f63115292
pbutler <pbutler@ea86384c-4743-4ab3-b769-c76ea7e5d1af>
2012-02-20 20:52:42 +0000

    added a new bootit script
    git-svn-id: file:///home/pbutler/svn/bootstrap@73 ea86384c-4743-4ab3-b769-c76ea7e5d1af

---
+++ bootit.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# vim: ts=4 sts=4 sw=4 tw=79 sta et
+"""%prog [options]
+Python source code - replace this with a description of the code and write the code below this text.
+"""
+
+__author__ = 'Patrick Butler'
+__email__  = 'pbutler@killertux.org'
+
+import os
+import sys
+import optparse
+
+def copy(src, dest):
+    os.link(src, dest)
+
+def main(args):
+    import  optparse
+    parser = optparse.OptionParser()
+    parser.usage = __doc__
+    parser.add_option("-q", "--quiet",
+                      action="store_false", dest="verbose", default=True,
+                      help="don't print status messages to stdout")
+    (options, args) = parser.parse_args()
+    if len(args) < 0:
+        parser.error("Not enough arguments given")
+    return 0
+
+
+
+if __name__ == "__main__":
+    import sys
+    sys.exit( main( sys.argv ) )
+
+
