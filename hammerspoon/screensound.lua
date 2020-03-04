3c64f20bdbea4a0d98bd64d514f9c0d897a7f45f
Patrick Butler <pbutler@killertux.org>
2020-03-04 16:30:49 -0500

    screensound now defaults to airpods if actice

--- hammerspoon/screensound.lua
+++ hammerspoon/screensound.lua
@@ -3,17 +3,21 @@ local screensound  = {}
 
 screensound.screen_watcher = hs.screen.watcher.new(function ()
   local int_audio = "MacBook Pro Speakers"
+  local airp_audio = "Patrick’s AirPods"
   local ext_audio = "LG UltraFine Display Audio"
   local ext_screen = "LG UltraFine"
+  local dev = nil
   if (hs.screen.primaryScreen():name() == ext_screen) then
-    local dev = hs.audiodevice.findDeviceByName(ext_audio)
+    dev = hs.audiodevice.findDeviceByName(ext_audio)
     -- hs.notify.new({title="Activating External Audio", informativeText=dev:name()}):send()
-    dev:setDefaultOutputDevice()
   else
-    local dev = hs.audiodevice.findDeviceByName(int_audio)
+    dev = hs.audiodevice.findDeviceByName(airp_audio)
+    if (dev == null) then
+      dev = hs.audiodevice.findDeviceByName(int_audio)
+    end
     -- hs.notify.new({title="Activating Internal Audio", informativeText=dev:name()}):send()
-    dev:setDefaultOutputDevice()
   end
+  dev:setDefaultOutputDevice()
 end):start()
 
 return screensound
