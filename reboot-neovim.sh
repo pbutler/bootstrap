4a48b5c3f02f901922f3ce8bb705943eb000e753
Patrick Butler <pbutler@killertux.org>
2020-03-04 22:50:30 -0500

    improvements for reboot-neovim.sh

--- reboot-neovim.sh
+++ reboot-neovim.sh
@@ -8,7 +8,8 @@ rm -rf ~/.cache/dein ~/.virtualenvs/neovim-py2 ~/.virtualenvs/neovim-py3
 
 python3 -mvenv ~/.virtualenvs/neovim-py3
 . ~/.virtualenvs/neovim-py3/bin/activate
-pip install --upgrade pip neovim
+pip install --upgrade pip
+pip install --upgrade neovim
 deactivate
 
 virtualenv -ppython2.7 ~/.virtualenvs/neovim-py2
