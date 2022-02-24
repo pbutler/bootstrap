608fbbdd7eea3c094f7ca65b03eef007675e60dd
Patrick Butler <pbutler@killertux.org>
2022-02-24 13:52:37 -0500

    lazy commit

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -75,7 +75,11 @@ return require('packer').startup(function(use)
       "catppuccin/nvim",
       as = "catppuccin",
       config = function ()
-        require('catppuccin').setup({})
+        require('catppuccin').setup({
+            lsp_trouble = true,
+            which_key = true,
+            lightspeed = true,
+          })
           vim.cmd[[colorscheme catppuccin]]
       end
     })
@@ -225,7 +229,17 @@ use {
 
   use 'andymass/vim-matchup'
   use 'sheerun/vim-polyglot'
-  use 'majutsushi/tagbar'
+  use({
+      'simrat39/symbols-outline.nvim',
+      requires = {
+        'neovim/nvim-lspconfig',
+      },
+      cmd = {
+        'SymbolsOutline',
+        'SymbolsOutlineOpen',
+        'SymbolsOutlineClose',
+      },
+    })
   use {
     'b3nj5m1n/kommentary',
     config = function()
@@ -268,15 +282,7 @@ use {
   use {'nvim-orgmode/orgmode',
     requires = "nvim-treesitter/nvim-treesitter",
     config = function()
-      local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-      parser_config.org = {
-        install_info = {
-          url = 'https://github.com/milisims/tree-sitter-org',
-          revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
-          files = {'src/parser.c', 'src/scanner.cc'},
-        },
-        filetype = 'org',
-      }
+      require('orgmode').setup_ts_grammar()
 
       require'nvim-treesitter.configs'.setup {
         -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
