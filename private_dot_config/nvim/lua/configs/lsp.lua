c24c0df6253052dc41cb3b97c48a5520c396690b
Patrick Butler <pbutler@killertux.org>
2022-11-11 09:44:22 -0500

    Fix cmp_nvim_lsp changes/deprecations

--- private_dot_config/nvim/lua/configs/lsp.lua
+++ private_dot_config/nvim/lua/configs/lsp.lua
@@ -30,7 +30,7 @@ local illuminate = require('illuminate')
 local navic = require('nvim-navic')
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-M.capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+M.capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local function set_commands()
   -- Commands.
