e0757f29cc52bf804c6823c7d8876b24061020d2
Patrick Butler <pabutler@vt.edu>
2024-09-27 11:39:25 -0400

    trying out nvim-tmux-nav

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -20,4 +20,28 @@ return {
       },
     },
   },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    keys = {
+      { "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>" },
+      { "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>" },
+      { "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>" },
+      { "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>" },
+      { "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>" },
+      { "<C-Space>", "<Cmd>NvimTmuxNavigateNext<CR>" },
+    },
+    config = function()
+      require("nvim-tmux-navigation").setup({
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        },
+      })
+    end,
+  },
 }
